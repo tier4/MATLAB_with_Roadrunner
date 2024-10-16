1126128d0d0e6510f23872565f0d7dd0abbaa633
児玉　英明（Kodama Hideaki） <cshd23008@g.nihon-u.ac.jp>
2024-10-16 11:40:13 +0900

    共有

--- hVehicle.m
+++ hVehicle.m
@@ -1,108 +1,81 @@
 classdef hVehicle < matlab.System
-
     % Copyright 2022 The MathWorks, Inc.
-    % properties (Access = private)
-    properties (Access = public)
-        mActorSimulationHdl; 
-        mScenarioSimulationHdl; 
-        mActor; 
+    properties (Access = private)
+        mActorSimulationHdl;
+        mScenarioSimulationHdl;
+        mActor;
         mLastTime = 0;
-        CurrentVelocity;
-        VelocityHistory = struct('time', {}, 'velocity', {}); % 新しいプロパティ
+        mJsonFilename = '/home/matsulab/Matlab/MATLAB/codes/test3.json'; % JSONファイル名
+        mFileId; % ファイルハンドル
+        mIsFirstEntry = true; % 最初のエントリかどうかを追跡
     end
-
     methods (Access=protected)
         function sz = getOutputSizeImpl(~)
             sz = [1 1];
         end
-
         function st = getSampleTimeImpl(obj)
             st = createSampleTime( ...
                 obj, 'Type', 'Discrete', 'SampleTime', 0.02);
         end
-
         function t = getOutputDataTypeImpl(~)
             t = "double";
         end
-
-        function resetImpl(obj)
-            obj.VelocityHistory = struct('time', {}, 'velocity', {}); % 履歴をリセット
-        end
-
         function setupImpl(obj)
             obj.mScenarioSimulationHdl = ...
                 Simulink.ScenarioSimulation.find( ...
-                    'ScenarioSimulation', 'SystemObject', obj);
-            
+                'ScenarioSimulation', 'SystemObject', obj);
             obj.mActorSimulationHdl = Simulink.ScenarioSimulation.find( ...
                 'ActorSimulation', 'SystemObject', obj);
-
             obj.mActor.pose = ...
                 obj.mActorSimulationHdl.getAttribute('Pose');
-
             obj.mActor.velocity = ...
                 obj.mActorSimulationHdl.getAttribute('Velocity');
+            % JSONファイルを初期化
+            obj.mFileId = fopen(obj.mJsonFilename, 'w');
+            fprintf(obj.mFileId, '[\n');
+            obj.mIsFirstEntry = true;
         end
-        
         function stepImpl(obj, ~)
             currentTime = obj.getCurrentTime;
             elapsedTime = currentTime - obj.mLastTime;
             obj.mLastTime = currentTime;
-
             velocity = obj.mActor.velocity;
-            % disp(velocity);
-            
-            % CurrentVelocityプロパティを更新
-            obj.CurrentVelocity = velocity;
-            % disp(obj.CurrentVelocity);
-            
-            % velocityの履歴を記録
-            obj.VelocityHistory(end+1).time = currentTime;
-            obj.VelocityHistory(end).velocity = velocity;
-
             pose = obj.mActor.pose;
-
             pose(1,4) = pose(1,4) + velocity(1) * elapsedTime; % x
             pose(2,4) = pose(2,4) + velocity(2) * elapsedTime; % y
             pose(3,4) = pose(3,4) + velocity(3) * elapsedTime; % z
-
+            disp(pose(1,4));
+            disp(pose(2,4));
+            disp(pose(3,4));
             obj.mActor.pose = pose;
-            
             obj.mActorSimulationHdl.setAttribute('Pose', pose);
-
-            jsonStr = jsonencode(obj.VelocityHistory);
-            fid = fopen('velocity_history.json', 'w');
-            if fid == -1
-                error('JSONファイルを作成できませんでした。');
-            end
-            fprintf(fid, '%s', jsonStr);
-            fclose(fid);
-            % disp('Velocity history saved to velocity_history.json');
+            % JSONにposeデータとvelocityデータを書き込む
+            obj.writePoseAndVelocityToJSON(pose, velocity, currentTime);
         end
-
         function releaseImpl(obj)
-            % シミュレーション終了時にJSONファイルを保存
-            obj.saveVelocityHistoryToJson();
-        end
-    end
-
-    % publicメソッドを追加してvelocityにアクセス
-    methods (Access = public)
-        function velocity = getVelocity(obj)
-            velocity = obj.CurrentVelocity;
-        end
-
-        function saveVelocityHistoryToJson(obj)
-            % VelocityHistoryをJSONファイルとして保存
-            jsonStr = jsonencode(obj.VelocityHistory);
-            fid = fopen('velocity_history.json', 'w');
-            if fid == -1
-                error('JSONファイルを作成できませんでした。');
+            % JSONファイルを閉じる
+            if ~isempty(obj.mFileId) && obj.mFileId ~= -1
+                fprintf(obj.mFileId, '\n]');
+                fclose(obj.mFileId);
             end
-            fprintf(fid, '%s', jsonStr);
-            fclose(fid);
-            disp('Velocity history saved to velocity_history.json');
+        end
+        function writePoseAndVelocityToJSON(obj, pose, velocity, currentTime)
+            % poseデータ、velocityデータ、タイムスタンプを構造体に格納
+            data = struct('timestamp', currentTime, ...
+                          'pose', struct('x', pose(1,4), 'y', pose(2,4), 'z', pose(3,4)), ...
+                          'velocity', struct('x', velocity(1), 'y', velocity(2), 'z', velocity(3)));
+            % データをJSONに変換
+            jsonStr = jsonencode(data);
+            % ファイルに書き込む
+            if ~obj.mIsFirstEntry
+                fprintf(obj.mFileId, ',\n');
+            else
+                obj.mIsFirstEntry = false;
+            end
+            fprintf(obj.mFileId, '%s', jsonStr);
+            % ファイルを閉じて再度開くことで、確実にディスクに書き込む
+            fclose(obj.mFileId);
+            obj.mFileId = fopen(obj.mJsonFilename, 'a');
         end
     end
-    
 end
